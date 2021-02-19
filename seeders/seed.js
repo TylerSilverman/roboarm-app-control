4d5d889067bb86071392840941f1c741e636c1c8
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-18 18:46:21 -0800

    Created seeds to the robot motion collection

---
+++ seeders/seed.js
@@ -0,0 +1,143 @@
+let moogose = require("mongoose");
+let db = require("../models");
+
+moogose.connect("mongodb://localhost/robotArm_db", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+let robotSeed = [
+  {
+    motorLocation: "base",
+    direction: "right",
+    motions: [
+      {
+        channel: 0,
+        pulse: 1000,
+      },
+    ],
+  },
+  {
+    motorLocation: "base",
+    direction: "left",
+    motions: [
+      {
+        channel: 0,
+        pulse: 2000,
+      },
+    ],
+  },
+  {
+    motorLocation: "shoulder",
+    direction: "down",
+    motions: [
+      {
+        channel: 1,
+        pulse: 1000,
+      },
+    ],
+  },
+  {
+    motorLocation: "shoulder",
+    direction: "up",
+    motions: [
+      {
+        channel: 1,
+        pulse: 2000,
+      },
+    ],
+  },
+  {
+    motorLocation: "elbow",
+    direction: "down",
+    motions: [
+      {
+        channel: 2,
+        pulse: 1000,
+      },
+    ],
+  },
+  {
+    motorLocation: "elbow",
+    direction: "up",
+    motions: [
+      {
+        channel: 2,
+        pulse: 2000,
+      },
+    ],
+  },
+  {
+    motorLocation: "wrist articulation",
+    direction: "down",
+    motions: [
+      {
+        channel: 3,
+        pulse: 1000,
+      },
+    ],
+  },
+  {
+    motorLocation: "wrist articulation",
+    direction: "up",
+    motions: [
+      {
+        channel: 3,
+        pulse: 2000,
+      },
+    ],
+  },
+  {
+    motorLocation: "wrist roll",
+    direction: "right",
+    motions: [
+      {
+        channel: 4,
+        pulse: 1000,
+      },
+    ],
+  },
+  {
+    motorLocation: "wrist roll",
+    direction: "left",
+    motions: [
+      {
+        channel: 4,
+        pulse: 2000,
+      },
+    ],
+  },
+  {
+    motorLocation: "claw",
+    direction: "open",
+    motions: [
+      {
+        channel: 3,
+        pulse: 1000,
+      },
+    ],
+  },
+  {
+    motorLocation: "claw",
+    direction: "close",
+    motions: [
+      {
+        channel: 3,
+        pulse: 2000,
+      },
+    ],
+  },
+];
+
+// Delete the collection Robot and insert this seed.
+db.Robot.deleteMany({})
+  .then(() => db.Robot.collection.insertMany(robotSeed))
+  .then((data) => {
+    //Log how many insertions were made to the database.
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
