14d28e063adc31c7821cf29a155579a929f2ef9f
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-20 10:07:25 -0800

    Created a new file to hold the robot motion controls functionality

---
+++ client/src/utils/pca9685.js
@@ -0,0 +1,113 @@
+import React, { useContext } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { Icon } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
+import MotionContext from "../utils/motionContext";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import API from "./apiHelper";
+import { io } from "socket.io-client";
+
+//Material UI style function
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  root: {
+    background: "linear-gradient(45deg, #63614f 30%, #3b3a32 90%)",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 40,
+    padding: "10px",
+    boxShadow: "0 3px 5px 2px rgba(59, 58, 50, .3)",
+    margin: "5px",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+}));
+
+// Create a variable that will pass the data to the claw server through socket.io
+const socket = io("http://localhost:3002/", {
+  withCredentials: true,
+});
+
+function Pca9685() {
+  // Material Ui Style function
+  const classes = useStyles();
+
+  // Grab the robot DB response from the Dashboard API call
+  const { robotMotions } = useContext(MotionContext);
+
+  // Function to grab information from the buttons and send to the motors through socket.io
+  const motionBtn = (e) => {
+    e.preventDefault();
+    // pwm stands for pulse width modulation. We are passing the pulse in microseconds.
+    socket.emit(
+      "pwmpulse",
+      e.currentTarget.getAttribute("channel"),
+      e.currentTarget.getAttribute("pulse")
+    );
+    console.log(
+      e.currentTarget.getAttribute("channel"),
+      e.currentTarget.getAttribute("pulse")
+    );
+  };
+
+  const favoritesBtn = (e) => {
+    e.preventDefault();
+    API.postFavorite({
+      motorLocation: e.currentTarget.getAttribute("location"),
+      direction: e.currentTarget.getAttribute("direction"),
+      motions: {
+        channel: e.currentTarget.getAttribute("channel"),
+        pulse: e.currentTarget.getAttribute("pulse"),
+      },
+    })
+      .then(console.log("Saved Favorite"))
+      .catch((err) => console.log({ err }));
+  };
+
+  return (
+    <div>
+      <ButtonGroup
+        orientation="vertical"
+        color="primary"
+        aria-label="vertical outlined primary button group"
+      >
+        {robotMotions.map((motion) => (
+          <div>
+            <Button
+              classes={{
+                root: classes.root, // class name, e.g. `classes-nesting-root-x`
+                label: classes.label, // class name, e.g. `classes-nesting-label-x`
+              }}
+              endIcon={<Icon>send</Icon>}
+              key={motion.id}
+              channel={motion.motions[0].channel}
+              pulse={motion.motions[0].pulse}
+              onClick={motionBtn}
+            >
+              {motion.motorLocation} - {motion.direction}
+            </Button>
+            <Button
+              classes={{
+                root: classes.root, // class name, e.g. `classes-nesting-root-x`
+                label: classes.label, // class name, e.g. `classes-nesting-label-x`
+              }}
+              startIcon={<SaveIcon />}
+              location={motion.motorLocation}
+              direction={motion.direction}
+              channel={motion.motions[0].channel}
+              pulse={motion.motions[0].pulse}
+              onClick={favoritesBtn}
+            />
+          </div>
+        ))}
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default Pca9685;
