67966fa6b35de337f7fb56bc8ff3e22678975fd6
Tyler Silverman <silverman.tyler@gmail.com>
2021-02-23 18:24:29 -0500

    edited style buttons

--- client/src/components/pages/SaveMotion.js
+++ client/src/components/pages/SaveMotion.js
@@ -1,35 +1,114 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import { Store } from "../../store";
+import { logoutUser } from "../../store/actions/authActions";
 import { Link } from "react-router-dom";
+import MotionContext from "../../utils/motionContext";
+import Pca9685 from "../../utils/pca9685";
+import API from "../../utils/apiHelper";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import robotImage from "../../assets/roboArm.png";
 
+const SaveMotion = (props) => {
+  const { state, dispatch } = useContext(Store);
+  const user = state.auth.user;
 
+  const [robotMotions, setMotions] = useState([]);
 
-function SaveMotion () {
-    return (
-      <div className="container valign-wrapper" style={{ height: '50vh' }}>
-      <div className="row">
-        <br></br>
-      <Link to="/Dashboard" role="button" className="btn btn-large waves-effect waves-light hoverable green accent-3">
-          Back to Dashboard !
+  useEffect(() => {
+    if (!state.auth.isAuthenticated) props.history.push("/login");
+
+    API.getUser()
+      .then((res) => console.log({ res }))
+      .catch((err) => console.log({ err }));
+  }, [state, props]);
+
+  // Run a GET request on the page load to grab the motor motions saved in the DB
+  useEffect(() => {
+    API.getRobotMotions()
+      .then((motions) => {
+        setMotions(motions.data);
+        console.log(motions.data);
+      })
+      .catch((err) => console.log({ err }));
+  }, []);
+  console.log(robotMotions);
+
+  const onLogoutClick = (e) => {
+    e.preventDefault();
+
+    logoutUser(props.history)(dispatch);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+    <MotionContext.Provider value={{ robotMotions }}>
+      <div className="container valign-wrapper" style={{ height: "auto" }}>
+        <div className="row">
+          <div>
+            <div>
+              <button
+            className="btn btn-med waves-effect waves-light hoverable blue accent-3"
+            style={{
+              width: "150px",
+              borderRadius: "3px",
+              letterSpacing: "1.5px",
+              marginTop: "1rem",
+            }}
+            onClick={onLogoutClick}
+          >
+            Logout
+          </button>
+          <Link to="/Dashboard" role="button" className="btn btn-med waves-effect waves-light hoverable blue accent-3" style={{
+              width: "150px",
+              borderRadius: "3px",
+              letterSpacing: "1.5px",
+              marginTop: "1rem",
+            }}>
+          Dashboard !
           </Link>
-        <div className="col s12 center-align">
+          <Link to="/saveMotion" role="button" className="btn btn-med waves-effect waves-light hoverable blue accent-3" style={{
+              width: "150px",
+              borderRadius: "3px",
+              letterSpacing: "1.5px",
+              marginTop: "1rem",
+            }}>
+          Saved Motions !
+          </Link>
+       
+          <h4>
+              <b>Welcome to the favorites Page!</b>
+            </h4>
+            <h5>
+              <b>{user.name.split(" ")[0]}</b>, your saved choices are below:
+            </h5>
+              <Grid
+                container
+                alignItems="flex-start"
+                lassName={classes.root}
+                spacing={1}
+              >
+                <Grid item justify="left" xs={6} spacing={6}>
+                  <img alt="logoRoboArm" src={robotImage} />
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+          <br></br><br></br><br></br><br></br><br></br>
           <br></br><br></br>
-          <h3>Welcome to the Save Motions Page</h3>
-         <div>
-          <br></br>
-          <h4>Save Selection Choices picked from previos page for the movements of the Claw</h4>
-        <button>Right/Left</button>
-        <button>Up/Down</button>
-        <button>Open Claw</button>
-        <button>Close Claw </button>
-        <button>Pre Saved</button>
-
-      </div>
-      
         </div>
+        <Grid item justify="right" xs={6} spacing={6}>
+          <Pca9685 />
+         </Grid>
       </div>
-    </div>
+    </MotionContext.Provider>
   );
-     
-}
+};
 
 export default SaveMotion;
--- client/src/components/partials/Footer.js
+++ client/src/components/partials/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Footer = props => {
+const Footer = (props) => {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
+    <footer >
       <p>&copy; {year} Camila Mimila and Tyler Silverman - RoboArm</p>
     </footer>
   );
