9bc3d3aa1a8094a596d15d09d9dae0298766dc01
Camila Alves Meyer <camila.alves85@gmail.com>
2021-03-01 06:39:53 -0800

    Added logo, and changed from Link to NavLink

--- client/src/components/partials/Navbar.js
+++ client/src/components/partials/Navbar.js
@@ -1,48 +1,75 @@
 import React, { useContext } from "react";
-import { Link, useHistory } from 'react-router-dom';
+import { NavLink, useHistory } from "react-router-dom";
 import { logoutUser } from "../../store/actions/authActions";
 import { Store } from "../../store";
+import logo from "../../assets/logo445.png";
 
-
-const Navbar = props => {
+const Navbar = (props) => {
   const history = useHistory();
   const { state, dispatch } = useContext(Store);
   const user = state.auth.user;
 
   const onLogoutClick = (e) => {
     e.preventDefault();
-  
+
     logoutUser(history)(dispatch);
   };
 
   return (
     <nav>
-    <div class="nav-wrapper white">
-      <Link to="/" className="brand-logo center black-text" style={{
-              width: "150px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              marginTop: "1rem",
-            }} style={{ fontFamily: 'monospace' }}>
-            <i className="material-icons"></i> RoboArm
-          </Link>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li> {state.auth.isAuthenticated? <a href="/" className="blue-text"
-            onClick={onLogoutClick}>
-             Logout
-          </a>: " "}</li>
-        
-        <li>  {state.auth.isAuthenticated? <Link to="/dashboard" className="blue-text">
-             Dashboard
-          </Link>: " "}</li>
-        
-          <li>  {state.auth.isAuthenticated? <Link to="/saveMotion" className="blue-text">
-             Favorites
-          </Link>: " "}</li>
-      </ul>
-    </div>
-  </nav>
- 
+      <div className="nav-wrapper white">
+        <NavLink
+          to="/"
+          className="brand-logo center"
+          style={{
+            borderRadius: "3px",
+            letterSpacing: "4px",
+            marginTop: "5px",
+            fontFamily: "Russo One, sans-serif",
+            fontSize: "45px",
+            color: "#3266A1",
+          }}
+        >
+          <span>
+            <img alt="logoRoboArm" src={logo} height="40px" width="40px" />{" "}
+          </span>
+          RoboArm
+        </NavLink>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          <li>
+            {state.auth.isAuthenticated ? (
+              <a href="/" className="blue-text" onClick={onLogoutClick}>
+                Logout
+              </a>
+            ) : (
+              " "
+            )}
+          </li>
+
+          <li>
+            {" "}
+            {state.auth.isAuthenticated ? (
+              <NavLink to="/dashboard" className="blue-text">
+                Dashboard
+              </NavLink>
+            ) : (
+              " "
+            )}
+          </li>
+
+          <li>
+            {" "}
+            {state.auth.isAuthenticated ? (
+              <NavLink to="/saveMotion" className="blue-text">
+                Favorites
+              </NavLink>
+            ) : (
+              " "
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
