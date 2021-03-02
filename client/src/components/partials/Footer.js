30cec5c475e81a9d5d4cc0360b417ef8e61b83e8
Camila Alves Meyer <camila.alves85@gmail.com>
2021-03-02 07:04:29 -0800

    Deleted unused import components and style

--- client/src/components/partials/Footer.js
+++ client/src/components/partials/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
-import { makeStyles, withTheme } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
